9a693e90fc8878af2f54bebbd60c9cd06f6c9c7f
twetto912 <franky85@hotmail.com.tw>
2023-07-21 03:38:44 +0800

    Revert back to HTTP, add semicolons

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const session = require('express-session')
+const express = require('express');
+const session = require('express-session');
 
-const mongoose = require('mongoose')
-const MongoStore = require('connect-mongo')
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
 
-const taskRouter = require('./routes/taskRoutes')
-const authRouter = require('./routes/authRoutes')
-const authController = require('./controllers/authController')
+const taskRouter = require('./routes/taskRoutes');
+const authRouter = require('./routes/authRoutes');
+const authController = require('./controllers/authController');
 
-const https = require('https')
-const fs = require('fs')
-
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
+});
 
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('Connected to MongoDB')
-})
+  console.log('Connected to MongoDB');
+});
 
-app.use(express.json()) // for parsing application/json
+app.use(express.json());  // for parsing application/json
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -41,25 +38,19 @@ app.use(session({
   }
 }));
 
-app.use(authController.passport.initialize())
-app.use(authController.passport.session())
+app.use(authController.passport.initialize());
+app.use(authController.passport.session());
 
-app.use('/auth', authRouter)
-app.use('/tasks', taskRouter)
+app.use('/auth', authRouter);
+app.use('/tasks', taskRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-const httpsOptions = {
-  key: fs.readFileSync('./private.key'),
-  cert: fs.readFileSync('./certificate.pem')
-};
-
-https.createServer(httpsOptions, app)
-.listen(3000, function() {
-  console.log('HTTPS server running on port 3000');
+  res.send('Hello World!');
 });
 
-module.exports = app
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+module.exports = app;
 
